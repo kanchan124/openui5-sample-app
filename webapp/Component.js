0e68f484c931037335dd6e6e9e6a1f5803c02c13
Kanchan Bawane <46624473+kanchan124@users.noreply.github.com>
2023-08-25 15:14:27 +0530

    environment set up and search field implementation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,4 +1,19 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
+sap.ui.loader.config({
+	map: {
+		"*": {
+			"@coveo/headless": "@coveo/headless/dist/browser/headless",
+		}
+	},
+	shim: {
+		"@coveo/headless/": {
+			"amd": true,
+			"deps": [],
+			"exports": "CoveoHeadless"
+		}
+	}
+});
+
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport", "@coveo/headless"], function(UIComponent) {
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
 		metadata: {
